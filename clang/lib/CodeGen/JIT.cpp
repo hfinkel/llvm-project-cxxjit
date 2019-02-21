4b71ca1f5ba4f2069aa42cbf41937a9fefb2a915
Hal Finkel <hfinkel@anl.gov>
2019-02-21 19:32:42 +0000

    working on AST loading, etc.

--- clang/lib/CodeGen/JIT.cpp
+++ clang/lib/CodeGen/JIT.cpp
@@ -14,14 +14,41 @@
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/DeclCXX.h"
 #include "clang/AST/DeclGroup.h"
+#include "clang/Basic/Diagnostic.h"
+#include "clang/Basic/DiagnosticIDs.h"
+#include "clang/Basic/DiagnosticOptions.h"
 #include "clang/Basic/FileManager.h"
+#include "clang/Basic/FileSystemOptions.h"
+#include "clang/Basic/LLVM.h"
+#include "clang/Basic/MemoryBufferCache.h"
 #include "clang/Basic/SourceManager.h"
 #include "clang/Basic/TargetInfo.h"
+#include "clang/Basic/TargetOptions.h"
 #include "clang/CodeGen/BackendUtil.h"
 #include "clang/CodeGen/ModuleBuilder.h"
+#include "clang/Driver/Compilation.h"
+#include "clang/Driver/Driver.h"
+#include "clang/Driver/Job.h"
+#include "clang/Driver/Options.h"
+#include "clang/Driver/Tool.h"
+#include "clang/Driver/ToolChain.h"
+#include "clang/Frontend/ASTUnit.h"
 #include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/CompilerInvocation.h"
 #include "clang/Frontend/FrontendDiagnostic.h"
+#include "clang/Frontend/FrontendOptions.h"
+#include "clang/Frontend/TextDiagnosticPrinter.h"
+#include "clang/Lex/HeaderSearch.h"
+#include "clang/Lex/HeaderSearchOptions.h"
 #include "clang/Lex/Preprocessor.h"
+#include "clang/Lex/PreprocessorOptions.h"
+#include "clang/Sema/Sema.h"
+#include "clang/Serialization/ASTReader.h"
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/IntrusiveRefCntPtr.h"
+#include "llvm/ADT/SmallString.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/ADT/Twine.h"
 #include "llvm/Bitcode/BitcodeReader.h"
 #include "llvm/CodeGen/MachineOptimizationRemarkEmitter.h"
 #include "llvm/IR/DebugInfo.h"
@@ -32,25 +59,299 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IRReader/IRReader.h"
 #include "llvm/Linker/Linker.h"
+#include "llvm/Option/ArgList.h"
+#include "llvm/Option/OptTable.h"
+#include "llvm/Option/Option.h"
 #include "llvm/Pass.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Host.h"
 #include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/Mutex.h"
+#include "llvm/Support/MutexGuard.h"
+#include "llvm/Support/Path.h"
+#include "llvm/Support/VirtualFileSystem.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/Timer.h"
 #include "llvm/Support/ToolOutputFile.h"
 #include "llvm/Support/YAMLTraits.h"
 #include "llvm/Transforms/IPO/Internalize.h"
 
+#include <cassert>
+#include <cstring>
 #include <memory>
+#include <string>
+#include <system_error>
+#include <utility>
+#include <vector>
 using namespace clang;
 using namespace llvm;
+
+#define DEBUG_TYPE "clang-jit"
+
+namespace {
+// FIXME: This is copied from lib/Frontend/ASTUnit.cpp
+
+/// Gathers information from ASTReader that will be used to initialize
+/// a Preprocessor.
+class ASTInfoCollector : public ASTReaderListener {
+  Preprocessor &PP;
+  ASTContext *Context;
+  HeaderSearchOptions &HSOpts;
+  PreprocessorOptions &PPOpts;
+  LangOptions &LangOpt;
+  std::shared_ptr<clang::TargetOptions> &TargetOpts;
+  IntrusiveRefCntPtr<TargetInfo> &Target;
+  unsigned &Counter;
+  bool InitializedLanguage = false;
+
+public:
+  ASTInfoCollector(Preprocessor &PP, ASTContext *Context,
+                   HeaderSearchOptions &HSOpts, PreprocessorOptions &PPOpts,
+                   LangOptions &LangOpt,
+                   std::shared_ptr<clang::TargetOptions> &TargetOpts,
+                   IntrusiveRefCntPtr<TargetInfo> &Target, unsigned &Counter)
+      : PP(PP), Context(Context), HSOpts(HSOpts), PPOpts(PPOpts),
+        LangOpt(LangOpt), TargetOpts(TargetOpts), Target(Target),
+        Counter(Counter) {}
+
+  bool ReadLanguageOptions(const LangOptions &LangOpts, bool Complain,
+                           bool AllowCompatibleDifferences) override {
+    if (InitializedLanguage)
+      return false;
+
+    LangOpt = LangOpts;
+    InitializedLanguage = true;
+
+    updated();
+    return false;
+  }
+
+  bool ReadHeaderSearchOptions(const HeaderSearchOptions &HSOpts,
+                               StringRef SpecificModuleCachePath,
+                               bool Complain) override {
+    this->HSOpts = HSOpts;
+    return false;
+  }
+
+  bool ReadPreprocessorOptions(const PreprocessorOptions &PPOpts, bool Complain,
+                               std::string &SuggestedPredefines) override {
+    this->PPOpts = PPOpts;
+    return false;
+  }
+
+  bool ReadTargetOptions(const clang::TargetOptions &TargetOpts, bool Complain,
+                         bool AllowCompatibleDifferences) override {
+    // If we've already initialized the target, don't do it again.
+    if (Target)
+      return false;
+
+    this->TargetOpts = std::make_shared<clang::TargetOptions>(TargetOpts);
+    Target =
+        TargetInfo::CreateTargetInfo(PP.getDiagnostics(), this->TargetOpts);
+
+    updated();
+    return false;
+  }
+
+  void ReadCounter(const serialization::ModuleFile &M,
+                   unsigned Value) override {
+    Counter = Value;
+  }
+
+private:
+  void updated() {
+    if (!Target || !InitializedLanguage)
+      return;
+
+    // Inform the target of the language options.
+    //
+    // FIXME: We shouldn't need to do this, the target should be immutable once
+    // created. This complexity should be lifted elsewhere.
+    Target->adjust(LangOpt);
+
+    // Initialize the preprocessor.
+    PP.Initialize(*Target);
+
+    if (!Context)
+      return;
+
+    // Initialize the ASTContext
+    Context->InitBuiltinTypes(*Target);
+
+    // Adjust printing policy based on language options.
+    Context->setPrintingPolicy(PrintingPolicy(LangOpt));
+
+    // We didn't have access to the comment options when the ASTContext was
+    // constructed, so register them now.
+    Context->getCommentCommandTraits().registerCommentOptions(
+        LangOpt.CommentOpts);
+  }
+};
+
+void fatal() {
+  report_fatal_error("Clang JIT failed!");
+}
+
+llvm::sys::SmartMutex<false> Mutex;
+
+struct CompilerData {
+  std::unique_ptr<CompilerInvocation>     Invocation;
+  std::unique_ptr<llvm::opt::OptTable>    Opts;
+  IntrusiveRefCntPtr<DiagnosticOptions>   DiagOpts;
+  std::unique_ptr<TextDiagnosticPrinter>  DiagnosticPrinter;
+  llvm::IntrusiveRefCntPtr<llvm::vfs::InMemoryFileSystem> InMemoryFileSystem;
+  IntrusiveRefCntPtr<DiagnosticsEngine>   Diagnostics;
+  IntrusiveRefCntPtr<FileManager>         FileMgr;
+  IntrusiveRefCntPtr<SourceManager>       SourceMgr;
+  IntrusiveRefCntPtr<MemoryBufferCache>   PCMCache;
+  std::unique_ptr<HeaderSearch>           HeaderInfo;
+  std::unique_ptr<PCHContainerReader>     PCHContainerRdr;
+  IntrusiveRefCntPtr<TargetInfo>          Target;
+  std::shared_ptr<Preprocessor>           PP;
+  IntrusiveRefCntPtr<ASTContext>          Ctx;
+  std::shared_ptr<clang::TargetOptions>   TargetOpts;
+  std::shared_ptr<HeaderSearchOptions>    HSOpts;
+  std::shared_ptr<PreprocessorOptions>    PPOpts;
+  IntrusiveRefCntPtr<ASTReader>           Reader;
+  std::unique_ptr<ASTConsumer>            Consumer;
+  std::unique_ptr<Sema>                   S;
+  TrivialModuleLoader                     ModuleLoader;
+
+  CompilerData(const void *CmdArgs, unsigned CmdArgsLen,
+               const void *ASTBuffer, size_t ASTBufferSize) {
+    StringRef CombinedArgv((const char *) CmdArgs, CmdArgsLen);
+    SmallVector<StringRef, 32> Argv;
+    CombinedArgv.split(Argv, '\0', /*MaxSplit*/ -1, false);
+
+    llvm::opt::ArgStringList CC1Args;
+    for (auto &ArgStr : Argv)
+      CC1Args.push_back(ArgStr.begin());
+
+    CC1Args.push_back("-fis-jit");
+
+    unsigned MissingArgIndex, MissingArgCount;
+    Opts = driver::createDriverOptTable();
+    llvm::opt::InputArgList ParsedArgs = Opts->ParseArgs(
+      CC1Args, MissingArgIndex, MissingArgCount);
+
+    DiagOpts = new DiagnosticOptions();
+    ParseDiagnosticArgs(*DiagOpts, ParsedArgs);
+    DiagnosticPrinter.reset(new TextDiagnosticPrinter(
+      llvm::errs(), &*DiagOpts));
+    Diagnostics = new DiagnosticsEngine(
+      IntrusiveRefCntPtr<DiagnosticIDs>(new DiagnosticIDs()), &*DiagOpts,
+      DiagnosticPrinter.get(), false);
+
+    Invocation.reset(new CompilerInvocation);
+    CompilerInvocation::CreateFromArgs(*Invocation,
+                                 const_cast<const char **>(CC1Args.data()),
+                                 const_cast<const char **>(CC1Args.data()) +
+                                 CC1Args.size(), *Diagnostics);
+
+    Invocation->getFrontendOpts().DisableFree = false;
+    Invocation->getCodeGenOpts().DisableFree = false;
+
+    InMemoryFileSystem = new llvm::vfs::InMemoryFileSystem;
+    FileMgr = new FileManager(FileSystemOptions(), InMemoryFileSystem);
+
+    const char *Filename = "__clang_jit.pcm";
+    StringRef ASTBufferSR((const char *) ASTBuffer, ASTBufferSize);
+    InMemoryFileSystem->addFile(Filename, 0,
+                                llvm::MemoryBuffer::getMemBufferCopy(ASTBufferSR));
+
+    PCHContainerRdr.reset(new RawPCHContainerReader);
+    SourceMgr = new SourceManager(*Diagnostics, *FileMgr,
+                                  /*UserFilesAreVolatile*/ false);
+    PCMCache = new MemoryBufferCache;
+    HSOpts = std::make_shared<HeaderSearchOptions>();
+    HSOpts->ModuleFormat = PCHContainerRdr->getFormat();
+    HeaderInfo.reset(new HeaderSearch(HSOpts,
+                                      *SourceMgr,
+                                      *Diagnostics,
+                                      *Invocation->getLangOpts(),
+                                      /*Target=*/nullptr));
+    PPOpts = std::make_shared<PreprocessorOptions>();
+
+    unsigned Counter;
+
+    PP = std::make_shared<Preprocessor>(
+        PPOpts, *Diagnostics, *Invocation->getLangOpts(),
+        *SourceMgr, *PCMCache, *HeaderInfo, ModuleLoader,
+        /*IILookup=*/nullptr,
+        /*OwnsHeaderSearch=*/false);
+
+    Ctx = new ASTContext(*Invocation->getLangOpts(), *SourceMgr,
+                         PP->getIdentifierTable(), PP->getSelectorTable(),
+                         PP->getBuiltinInfo());
+
+    Reader = new ASTReader(*PP, Ctx.get(), *PCHContainerRdr, {},
+                           /*isysroot=*/"",
+                           /*DisableValidation=*/ false,
+                           /*AllowPCHWithCompilerErrors*/ false);
+
+    Reader->setListener(llvm::make_unique<ASTInfoCollector>(
+      *PP, Ctx.get(), *HSOpts, *PPOpts, *Invocation->getLangOpts(),
+      TargetOpts, Target, Counter));
+
+    Ctx->setExternalSource(Reader);
+
+    switch (Reader->ReadAST(Filename, serialization::MK_MainFile,
+                            SourceLocation(), ASTReader::ARR_None)) {
+    case ASTReader::Success:
+      break;
+
+    case ASTReader::Failure:
+    case ASTReader::Missing:
+    case ASTReader::OutOfDate:
+    case ASTReader::VersionMismatch:
+    case ASTReader::ConfigurationMismatch:
+    case ASTReader::HadErrors:
+      Diagnostics->Report(diag::err_fe_unable_to_load_pch);
+      fatal();
+      return;
+    }
+
+    PP->setCounterValue(Counter);
+
+    // TODO: Make a use consumer...
+    Consumer.reset(new ASTConsumer);
+
+    // Create a semantic analysis object and tell the AST reader about it.
+    S.reset(new Sema(*PP, *Ctx, *Consumer));
+    S->Initialize();
+    Reader->InitializeSema(*S);
+
+    // Tell the diagnostic client that we have started a source file.
+    Diagnostics->getClient()->BeginSourceFile(PP->getLangOpts(), PP.get());
+  }
+};
+
+llvm::DenseMap<const void *, std::unique_ptr<CompilerData>> TUCompilerData;
+
+} // anonymous namespace
 
 extern "C"
 #ifdef _MSC_VER
 __declspec(dllexport)
 #endif
-void *__clang_jit(void *CmdArgs, unsigned CmdArgsLen,
-                  void *ASTBuffer, size_t ASTBufferSize,
-                  void *NTTPValues, unsigned Idx) {
+void *__clang_jit(const void *CmdArgs, unsigned CmdArgsLen,
+                  const void *ASTBuffer, size_t ASTBufferSize,
+                  const void *NTTPValues, unsigned Idx) {
+  llvm::MutexGuard Guard(Mutex);
+
+  CompilerData *CD;
+  auto TUCDI = TUCompilerData.find(ASTBuffer);
+  if (TUCDI == TUCompilerData.end()) {
+    CD = new CompilerData(CmdArgs, CmdArgsLen, ASTBuffer, ASTBufferSize);
+    TUCompilerData[ASTBuffer].reset(CD);
+  } else {
+    CD = TUCDI->second.get();
+  }
+
   return 0;
 }
 
